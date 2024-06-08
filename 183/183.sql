9528dcf272752d44b9e1fde1dfe7d7160c592668
Matt Wilcox <mewazhere@gmail.com>
2024-06-08 18:40:03 -0500

    183, 1757 SQL Practice

---
+++ 183/183.sql
@@ -0,0 +1,34 @@
+-- 183. Customers Who Never Order
+-- Table: Customers
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table indicates the ID and name of a customer.
+ 
+
+-- Table: Orders
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | customerId  | int  |
+-- +-------------+------+
+-- id is the primary key (column with unique values) for this table.
+-- customerId is a foreign key (reference columns) of the ID from the Customers table.
+-- Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+
+-- Write a solution to find all customers who never order anything.
+
+-- Return the result table in any order.
+
+SELECT Customers.name as Customers
+FROM Customers
+LEFT JOIN Orders ON Customers.id = Orders.customerid
+WHERE Orders.id IS NULL;
