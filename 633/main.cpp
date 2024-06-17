cb0c32f3bca4ef8bbcf1cc2505f76cd7e4ef72ad
Matt Wilcox <46714924+enjamb-ment@users.noreply.github.com>
2024-06-16 23:07:08 -0500

    added header - question details

--- 633/main.cpp
+++ 633/main.cpp
@@ -1,3 +1,7 @@
+/* 633. Sum of Square Numbers
+Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+*/
+
 class Solution {
 public:
     bool judgeSquareSum(int c) {
