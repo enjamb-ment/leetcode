6aa52667b59eb8b954cd35ccb13f4ae1cf13c732
Matt Wilcox <46714924+enjamb-ment@users.noreply.github.com>
2024-06-20 15:19:21 -0500

    added another method that essentially did the same thing.

--- 2894/main.cpp
+++ 2894/main.cpp
@@ -28,6 +28,24 @@ public:
     }
 };
 
+class Solution2 {    //did it essentially the same way again.  Didn't realize I'd already done it.
+public:
+    int differenceOfSums(int n, int m) {
+        int num1 = 0, num2 = 0;
+
+        for (int i = 0; i < n+1; i++)
+        {
+            if (i % m == 0)
+            {
+                num2 += i;
+            } else
+            {
+                num1 += i;
+            }
+        }
+        return num1 - num2;
+    }
+};
 
 // Faster solution - not mine:
 class LeetCodeSolution {
