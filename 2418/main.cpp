583d409c05b910c10d607448667ca06ac0b783ae
Matt Wilcox <46714924+enjamb-ment@users.noreply.github.com>
2024-07-22 10:17:17 -0500

    Update main.cpp - C++ unordered map

--- 2418/main.cpp
+++ 2418/main.cpp
@@ -9,13 +9,22 @@
 #include <iostream>
 #include <vector>
 
-
 class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        unordered_map<int, string> heightAndName;
 
+        for (int i = 0; i < names.size(); i++) {
+            heightAndName[heights[i]] = names[i];
+        }
+
+        sort(heights.begin(), heights.end());
+
+        vector<string> namesByHeight(names.size());
+
+        for (int i = names.size() - 1; i >= 0; i--) {
+            namesByHeight[names.size() - i - 1] = heightAndName[heights[i]];
+        }
+        return namesByHeight;
+    }
 };
-
-int main()
-{
-
-
-}
